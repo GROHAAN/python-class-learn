eb8cdfc122c3accaa917f0755a6421125fa11862
GROHAAN <ag871998@gmail.com>
2025-10-28 12:53:02 +0530

    some functions learn

---
+++ func.py
@@ -0,0 +1,61 @@
+# def display():
+#     print('hello')
+# print(display())
+
+# def display():
+#     print('hello')
+# display()
+
+# def display():
+#     print('hello')
+# x = display()
+
+# def display():
+#     print('hello')
+# x = display()
+# print(x)
+# print(x)
+# print(x)
+
+# def display():
+#     return "hello"
+# print(display())
+
+# def display():
+#     return "hello"
+# x = display()
+
+
+# def display():
+#     return 
+# x = display()
+# print(x)
+# print(x)
+# print(x)
+
+# Positional arguments
+# def fun_name (x,y,z):
+#     print(f'x= {x} y= {y} z= {z}')
+# p = int(input('enter any value: '))
+# q = int(input('enter any value: '))
+# r = int(input('enter any value: '))
+
+# fun_name(p,q,r)
+
+
+# default Arguments
+# def fun_name (x= 0,y= 0,z= 0):
+#     print(f'x= {x} y= {y} z= {z}')
+# # fun_name(10,20,30)
+# # fun_name()
+# fun_name(10,20)
+
+# vairable length positional arguments
+
+# def fun_name(* args):
+#     print(args)
+#     print(type(args))
+# t = eval(input("enter any tuple: "))
+# fun_name(t)
+# # fun_name(10,20,30,'python',49,50)
+
