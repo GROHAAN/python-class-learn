ffd5dc6941a39b4643347457cb95b2aa6e6eb4b0
GROHAAN <ag871998@gmail.com>
2025-11-06 12:41:45 +0530

    generator learn

---
+++ generator.py
@@ -0,0 +1,19 @@
+# def data():
+#     for i in range(1,10):
+#         yield i
+# x = data()
+# # for i in x:
+# #     print(i)
+# print(next(x))
+# print(next(x))
+# print('helllo')
+# print(next(x))
+# for i in x:
+#     print(i)
+
+l = [1,2,3,4,6,'python']
+l1 = iter(l)
+print(l1)
+print(next(l1))
+
+
